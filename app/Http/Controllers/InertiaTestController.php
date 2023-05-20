4430eb8baa69722bfd03413da12d4fed910c80e3
ArataYoshiba <yoshibaarata@icloud.com>
2023-05-21 01:33:53 +0900

    商品関連

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -43,7 +43,8 @@ class InertiaTestController extends Controller
         $inertiaTest->content = $request->content;
         $inertiaTest->save();
 
-        return to_route('inertia.index')->with([
+        return to_route('inertia.index')
+        ->with([
             'message' => '登録しました'
         ]);
     }
