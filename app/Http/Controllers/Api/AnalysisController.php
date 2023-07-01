ddd6d176993fa686240dd23bab5626adaf7adb76
ArataYoshiba <yoshibaarata@icloud.com>
2023-07-01 19:00:29 +0900

    rfm

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -9,6 +9,7 @@ use App\Models\Order;
 use Illuminate\Support\Facades\DB;
 use App\Services\AnalysisService;
 use App\Services\DecileService;
+use App\Services\RFMService;
 
 
 class AnalysisController extends Controller
@@ -37,12 +38,18 @@ class AnalysisController extends Controller
             list($data, $labels, $totals) = DecileService::decile($subQuery);
         }
 
-        // Ajax通信なのでJsonで返却する必要がある
-        return response()->json([
-            'data' => $data,
-            'type' => $request->type,
-            'labels' => $labels,
-            'totals' => $totals
-        ], Response::HTTP_OK);
+        if ($request->type === 'rfm')
+        {
+            list($data,$totals,$eachCount) = RFMService::rfm($subQuery, $request->rfmPrms);
+            
+            // Ajax通信なのでJsonで返却する必要がある
+            return response()->json([
+                'data' => $data,
+                'type' => $request->type,
+                'eachCount' => $eachCount,
+                'totals' => $totals
+            ], Response::HTTP_OK);
+        }
+
     }
 }
